825ff5835b4fb5efc02aba518f7f004edfdcd35e
unknown <rajafakhrir7@gmail.com>
2025-03-27 11:48:27 +0700

    Ini Update Baru

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/template','template');
+
+Route::controller(\App\Http\Controllers\UserController::class)->group(function (){
+    Route::get('/login','login');
+    Route::post('/login','doLogin');
+    Route::post('/logout','doLogout');
+
+});
---
+++ tests/Feature/UserServiceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    private UserService $userService;
+
+    protected function setUp():void
+    {
+        parent::setUp();
+
+        $this->userService = $this->app->make(UserService::class);
+    }
+
+    public function testLoginSuccess()
+    {
+        self::assertTrue($this->userService->login("raja","12345"));
+    }
+
+    public function testLoginNotFound()
+    {
+        self::assertFalse($this->userService->login("Yoga","Yoga"));
+
+    }
+
+    public function testWrongPassword()
+    {
+        self::assertFalse($this->userService->login("raja","Yoga"));
+    }
+
+    public function testLoginPage()
+    {
+        $this->get('/login')
+        ->assertSeeText("Login");
+    }
+}
